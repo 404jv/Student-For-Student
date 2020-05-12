e01cdf62125f920e16082fc3a6d0c09335623678
Ezever <joaovictorramalho7@gmail.com>
2020-05-12 18:18:50 -0300

    Adcionando conexão com o banco de dados

---
+++ backend/knexfile.js
@@ -0,0 +1,37 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'sfs_development',
+      user: 'postgres',
+      password: 'frances321321'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+
+  /*production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }*/
+
+};
