49741bf06b38160e58a6dd19b3cf8a364ee33c36
Ezever <joaovictorramalho7@gmail.com>
2020-05-13 12:42:56 -0300

    Rotas para topics

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const routes = require('express').Router();
 const UsersController = require('./controller/UsersController');
+const TopicsController = require('./controller/TopicsController');
 
 routes
   .get('/users', UsersController.index)
   .post('/users', UsersController.store)
   .put('/users/:id', UsersController.update)
-  .delete('/users/:id', UsersController.destroy);
+  .delete('/users/:id', UsersController.destroy)
+
+  .get('/topics',  TopicsController.index)
+  .post('/topics',  TopicsController.store);
 
 module.exports = routes;
