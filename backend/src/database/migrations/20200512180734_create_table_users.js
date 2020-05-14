3772e80afa12baa738fa3a8255476cf4e569b93f
Ezever <joaovictorramalho7@gmail.com>
2020-05-14 18:50:00 -0300

    Renomendo update para updated

--- backend/src/database/migrations/20200512180734_create_table_users.js
+++ backend/src/database/migrations/20200512180734_create_table_users.js
@@ -6,7 +6,7 @@ exports.up = knex => knex.schema.createTable('users', table => {
   table.string('password', 16).notNullable();
 
   table.timestamp('created_at').defaultTo(knex.fn.now());
-  table.timestamp('update_at').defaultTo(knex.fn.now());
+  table.timestamp('updated_at').defaultTo(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable('users');
