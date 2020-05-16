bb0b484464e15bc11fdfa905a0c60668b694e233
Ezever <joaovictorramalho7@gmail.com>
2020-05-16 20:47:14 -0300

    Adicionando column para gerenciar próximo estudo

--- backend/src/database/migrations/20200515184255_create_table_matter.js
+++ backend/src/database/migrations/20200515184255_create_table_matter.js
@@ -5,6 +5,7 @@ exports.up = async knex => knex.schema.createTable('matter', table => {
   table.string('title', 35).notNullable();
   table.string('resume', 254).notNullable();
   table.string('tags', 10);
+  table.timestamp('nextStudy').defaultTo(knex.fn.now());
 
   table.string('topic_id')
     .references('topics.id')
