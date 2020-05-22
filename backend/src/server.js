a267ec3de25d4ce9a6d1fe7ae49bb046cb23bfc8
Ezever <joaovictorramalho7@gmail.com>
2020-05-21 23:57:44 -0300

    tratando errors com celebrate

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
