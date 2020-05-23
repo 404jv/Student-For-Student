8ed660080d3436d23179d78983f26e0aeb8f2c9b
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-05-23 20:14:34 -0300

    aumentando quantia de caracteres para senha

--- backend/src/database/migrations/20200512180734_create_table_users.js
+++ backend/src/database/migrations/20200512180734_create_table_users.js
@@ -4,7 +4,7 @@ exports.up = async knex => knex.schema.createTable('users', table => {
   table.string('id', 8).primary();
   table.string('name', 100).notNullable();
   table.string('email', 254).unique().notNullable();
-  table.string('password', 16).notNullable();
+  table.string('password', 100).notNullable();
 
   table.timestamp('created_at').defaultTo(knex.fn.now());
   table.timestamp('updated_at').defaultTo(knex.fn.now());
