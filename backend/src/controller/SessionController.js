99495c37c4df7ed608c1f4eef96dc7fec27f4650
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-05-25 19:35:13 -0300

    Criando nova sessão com token

--- backend/src/controller/SessionController.js
+++ backend/src/controller/SessionController.js
@@ -3,25 +3,32 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+function generateToken(params = {}) {
+  return jwt.sign({
+    id: params.id,
+  }, process.env.JWT_KEY, { expiresIn: 86400 });
+}
+
 module.exports = {
-  async store(req, res) {
-    const { email, password } = req.body;
+  async store(req, res, next) {
+    try {
+      const { email, password } = req.body;
 
-    const user = await knex('users')
-      .where({ email });
-    
-    const isUser = await bcrypt.compare(password, user[0].password);
+      const user = await knex('users')
+        .where({ email });
+      
+      const isUser = await bcrypt.compare(password, user[0].password);
 
-    if (!isUser) 
-      return res.status(401).send({ message: 'No user found with this credential' });
-
-    const { id, name } = await user;
-    const token = jwt.sign({
-      id,
-      name,
-      email
-    }, process.env.JWT_KEY, { expiresIn: '75h' });
-
-    return res.json({ token: token });
+      if (!isUser) 
+        return res.status(401).send({ message: 'No user found with this credential' });
+ 
+      
+      return res.json({
+        user: user[0],
+        token: generateToken({ id: user[0].id }),
+      });
+    } catch (error) {
+        next(error);
+    }
   }
 }
