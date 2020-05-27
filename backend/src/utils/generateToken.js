fe42bdd61a273979c69865673587efaa8de6656d
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-05-27 20:17:35 -0300

    gerando token com name e email

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -4,5 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (params = {}) => {
   return jwt.sign({
     id: params.id,
+    name: params.name,
+    email: params.email,
   }, process.env.JWT_KEY);
 }
