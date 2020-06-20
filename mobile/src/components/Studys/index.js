d6877c7cde54a26dd73e54c6b6b171e3fa391c53
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-06-19 22:09:18 -0300

    Refatorando Home em component Study para a listagem dos studys na Home

---
+++ mobile/src/components/Studys/index.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import { 
+  Text, 
+  View, 
+  ScrollView, 
+  TouchableOpacity, 
+} from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold 
+} from '@expo-google-fonts/roboto';
+
+import styles from './style';
+import api from '../../services/api';
+
+export default function Home() {
+  const [studys, setStudys] = useState([]);
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+  
+  useEffect(() => {
+    api.get('study/all', {
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMzMjJmZDZjIiwibmFtZSI6InRlc3QxIiwiZW1haWwiOiJ0ZXN0MUAiLCJpYXQiOjE1OTA2MjI5MDB9.2vA8PD21i5F5ZQkPztgXOOCv4idU8q4gQvHEyBFt2k8'
+      },
+    }).then(res => {
+      setStudys(res.data);
+    });
+  }, []);
+
+  if (!fontsLoaded)
+    return <AppLoading />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.groupTopics}>
+        {studys.map(study => (
+          <View key={study.id} style={study.topic}>
+            <Text style={styles.topicTitle}>{study.topicName}</Text>
+            <ScrollView
+              style={styles.groupMatter}
+              contentContainerStyle={{
+                contentContainerStyle: 32
+              }}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+            >
+              {
+                study.matters.length === 0
+
+                ? <View style={[styles.matter, { justifyContent: 'center' }]}>
+                    <Icon 
+                      name="plus" 
+                      size={50}
+                    />
+                  </View> 
+                : study.matters.map(matter => (
+                  <TouchableOpacity 
+                      key={matter.id} 
+                      style={styles.matter}
+                      activeOpacity={0.6}
+                    >
+                      <Text 
+                        style={styles.matterTitle}
+                      >{matter.title.substring(0, 11)}
+                      </Text>
+                      <Text 
+                        style={styles.matterTags}
+                      >{matter.nextStudy}
+                      </Text>
+                    </TouchableOpacity>
+                ))
+              }
+            </ScrollView>
+            <TouchableOpacity 
+              style={styles.button}
+              activeOpacity={0.6}
+            >
+              <Text style={styles.textButton}>Ver tudo</Text>
+              <Icon 
+                name="arrow-right" 
+                size={18} 
+                color="#0A0B0D"
+                style={{
+                  marginRight: 8
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        ))
+      }
+      </View>
+    </View>
+  );
+}
---
+++ mobile/src/components/Studys/style.js
@@ -0,0 +1,71 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold 
+} from '@expo-google-fonts/roboto';
+
+export default StyleSheet.create({
+
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight + 5,
+    backgroundColor: '#FFF'
+  },
+
+  topic: {
+    marginBottom: 20
+  },
+
+  topicTitle: {
+    fontWeight: 'bold',
+    fontFamily: 'Roboto_700Bold',
+    fontSize: 20,
+    marginLeft: 20
+  },
+
+  groupMatter: {
+    flexDirection: "row",
+    marginTop: 16,
+    marginBottom: 8,
+  },
+
+  matter: {
+    margin: 8,
+    borderWidth: 2,
+    borderColor: '#525759',
+    borderRadius: 8,
+    height: 90,
+    width: 90,
+    alignItems: 'center',
+  },
+
+  matterTitle: {
+    textAlign: 'center',
+    fontSize: 25,
+    fontFamily: 'Roboto_400Regular'
+  },
+
+  matterTags: {
+    textAlign: 'center',
+    fontSize: 10,
+    fontFamily: 'Roboto_400Regular'
+  },
+
+  button: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginRight: 8,
+    borderBottomColor: '#525759',
+    alignItems: 'center',
+  },
+
+  textButton: {
+    marginLeft: 10,
+    fontSize: 16,
+    textAlign: 'center'
+  }
+
+});
