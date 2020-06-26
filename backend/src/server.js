324c64b12b0a005904ab427369990435853637f1
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-06-25 23:25:28 -0300

    tratando arquivos statics com express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const { errors } = require('celebrate');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 app.use(errors());
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
