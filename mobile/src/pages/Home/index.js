efad392e0581290cd2e617d3d4987ac9732a6d22
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-06-27 19:37:50 -0300

    Removendo gambiarra de navegação entre telas

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -4,49 +4,48 @@ import {
   TouchableOpacity, 
   TextInput 
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
 import { Feather as Icon } from '@expo/vector-icons';
 
 import styles from './style';
 
 import Studys from '../../components/Studys';
-import SearchTopic from '../../components/SearchTopic';
 
 export default function Home() {
-  const [selectedInput, setSelectedInput] = useState(false);
+  const navigation = useNavigation();
+
+  function handleNavigateToSearchTopic() {
+    navigation.navigate('SearchTopic');
+  }
 
   return (
     <View style={styles.container}>
-      {selectedInput
-        ? <SearchTopic handleSelectedInput={setSelectedInput} />
-        : <>
-            <View style={styles.searchContainer}>
-              <Icon 
-                name="search"
-                size={20}
-                style={styles.searchIcon}
-              />
-              <TextInput 
-                style={styles.input}
-                placeholder="Procurar um tópico"
-                underlineColorAndroid="transparent"
-                onTouchStart={() => setSelectedInput(true)}
-              />
-              
-              <TouchableOpacity
-                activeOpacity={0.6}
-              >
-                <Icon 
-                  name="x"
-                  size={20}
-                  style={styles.searchIcon}
-                  color="#808080"
-                />
-              </TouchableOpacity>
-            </View>
+      <View style={styles.searchContainer}>
+        <Icon 
+          name="search"
+          size={20}
+          style={styles.searchIcon}
+        />
+        <TextInput 
+          style={styles.input}
+          placeholder="Procurar um tópico"
+          underlineColorAndroid="transparent"
+          onTouchStart={handleNavigateToSearchTopic}
+        />
+        
+        <TouchableOpacity
+          activeOpacity={0.6}
+        >
+          <Icon 
+            name="x"
+            size={20}
+            style={styles.searchIcon}
+            color="#808080"
+          />
+        </TouchableOpacity>
+      </View>
 
-            <Studys />
-         </>
-      }
+      <Studys />
     </View>
   );
 }
