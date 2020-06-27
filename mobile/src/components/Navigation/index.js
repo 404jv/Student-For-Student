3a735d598bea897361351524e5ba04c1103e38a0
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-06-27 19:37:04 -0300

    Criando novo component de navegação

---
+++ mobile/src/components/Navigation/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Menu from "../Menu";
+import SearchTopic from '../SearchTopic';
+
+const AppStack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <AppStack.Navigator
+      headerMode="none"
+    >
+      <AppStack.Screen name="Menu"        component={Menu}/>
+      <AppStack.Screen name="SearchTopic" component={SearchTopic}/>
+    </AppStack.Navigator>
+  );
+};
+
+export default Navigation;
