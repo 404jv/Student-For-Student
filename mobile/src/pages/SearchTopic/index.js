e735b1bc4e68ff7e212a6df9175bb71d17a036c7
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-06-28 15:25:30 -0300

    Trocando localização do SearchTopic

---
+++ mobile/src/pages/SearchTopic/index.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { 
+  View, 
+  TouchableOpacity, 
+  TextInput,
+  Text,
+  Image,
+  FlatList
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native'
+import { Feather as Icon } from '@expo/vector-icons';
+
+import styles from './style';
+import api from '../../services/api';
+
+export default function SearchTopic() {
+  const [findTopic, setFindTopic] = useState('');
+  const [findTopics, setFindTopics] = useState([]);
+
+  const navigation = useNavigation();
+
+  function handleClearInput() {
+    setFindTopic('');
+  }
+
+  function handlefindTopic() {
+    api.get('/topics/find', {
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjljYTBlNWNhIiwibmFtZSI6InRlc3QxMzIiLCJlbWFpbCI6InRlc3QzMjFAIiwiaWF0IjoxNTkzMTQwNzg1fQ.gl1AJJC5UrqzErwjRW2Y0ObrpjqI3oCB1gs7Joxrm60'
+      },
+      params: {
+        name: findTopic
+      }
+    }).then(res => {
+      setFindTopics(res.data);
+    })
+  }
+
+  function handleNavigateBack() {
+    navigation.goBack('Home')
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          onPress={handleNavigateBack}
+        >
+          <Icon 
+            name="arrow-left"
+            size={20}
+            style={styles.searchIcon}
+          />
+        </TouchableOpacity>
+        <TextInput 
+          style={styles.input}
+          placeholder="Procurar um tópico"
+          underlineColorAndroid="transparent"
+          onChangeText={setFindTopic}
+          onChange={handlefindTopic}
+          value={findTopic}
+          autoFocus
+        />
+        
+        <TouchableOpacity
+          onPress={handleClearInput}
+          activeOpacity={0.6}
+        >
+          <Icon 
+            name="x"
+            size={20}
+            style={styles.searchIcon}
+            color="#808080"
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.lineBottom} />
+
+      <FlatList 
+        data={findTopics}
+        style={styles.groupTopics}
+        keyExtractor={topic => String(topic.id)}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        onEndReachedThreshold={0.2}
+        renderItem={({ item: topic }) => (
+          <View style={styles.topic}>
+            <TouchableOpacity 
+              activeOpacity={0.6}
+            >
+              <Image 
+                source={{ uri: topic.image_url}}
+                style={styles.topicImage}
+              />
+              <View style={styles.topicInfo}>
+              <Text style={styles.topicName}>{topic.name}</Text>
+                <Icon 
+                  name="arrow-right"
+                  size={20}
+                  style={styles.button}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
