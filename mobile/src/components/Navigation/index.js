4683409515a9eb50694e668101ebbd9641a31369
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-07-03 21:50:49 -0300

    Add Detail para navegação

--- mobile/src/components/Navigation/index.js
+++ mobile/src/components/Navigation/index.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Menu from "../Menu";
 import SearchTopic from '../../pages/SearchTopic';
 import Matters from '../../pages/Matters';
+import Detail from '../../pages/Detail';
 
 const AppStack = createStackNavigator();
 
@@ -15,6 +16,7 @@ const Navigation = () => {
       <AppStack.Screen name="Menu"        component={Menu}/>
       <AppStack.Screen name="SearchTopic" component={SearchTopic}/>
       <AppStack.Screen name="Matters"     component={Matters}/>
+      <AppStack.Screen name="Detail"      component={Detail}/>
     </AppStack.Navigator>
   );
 };
