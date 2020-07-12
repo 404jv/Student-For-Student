e7c4c26742c1035a5a202925afbbe05755f49b7d
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-07-12 17:54:08 -0300

    Add CreateMatter para navegação

--- mobile/src/components/Navigation/index.js
+++ mobile/src/components/Navigation/index.js
@@ -5,6 +5,7 @@ import Menu from "../Menu";
 import SearchTopic from '../../pages/SearchTopic';
 import Matters from '../../pages/Matters';
 import Detail from '../../pages/Detail';
+import CreateMatter from '../../pages/CreateMatter';
 
 const AppStack = createStackNavigator();
 
@@ -17,6 +18,7 @@ const Navigation = () => {
       <AppStack.Screen name="SearchTopic" component={SearchTopic}/>
       <AppStack.Screen name="Matters"     component={Matters}/>
       <AppStack.Screen name="Detail"      component={Detail}/>
+      <AppStack.Screen name="CreateMatter" component={CreateMatter}/>
     </AppStack.Navigator>
   );
 };
