261145addd14c6075884f4cc4c428ff75e7e494a
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-07-13 21:25:36 -0300

    Adicionando frases aleatórias

--- mobile/src/pages/CreateMatter/index.js
+++ mobile/src/pages/CreateMatter/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { Text, View, TextInput } from 'react-native';
 import { useRoute } from '@react-navigation/native'
+import { Feather as Icon } from '@expo/vector-icons';
 
 import styles from './style';
+
+
 
 export default function InputMatter() {
 
   const route = useRoute();
   const topic_id = route.params.topic_id;
+  const pharases = [
+    '"O aprendizado é conhecimento, e conhecimento é liberdade e poder." - Altaïr',
+    '"Os investimentos em conhecimento geram os melhores dividendos" - Benjamin Franklin',
+    '"Lembre-se que as pessoas podem tirar tudo de você menos o seu conhecimento" - Albert Einstein',
+    '"Na ciência temos de nos interessar pelas coisas e não pelas pessoas." - Marie Curie',
+  ];
+  const numberPharase = Math.floor(Math.random() * pharases.length);
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <View 
+          style={styles.iconContainer}
+          activeOpacity={0.6}
+          onPress={() => handleNavigateToCreateMatter(study.id)}
+        >
+          <Icon 
+            name="book"
+            size={40}
+          />
+        </View>
+        <Text style={styles.headerTitle}>Continue aprendendo!</Text>
+        <Text style={styles.headerText}>{pharases[numberPharase]}</Text>
+      </View>
       <TextInput
         placeholder="Título"
         underlineColorAndroid="transparent"
--- mobile/src/pages/CreateMatter/style.js
+++ mobile/src/pages/CreateMatter/style.js
@@ -6,5 +6,48 @@ export default StyleSheet.create({
     flex: 1,
     paddingTop: Constants.statusBarHeight + 10,
     backgroundColor: '#FFF',
-  }
+    alignItems: 'center'
+  },
+
+  iconContainer: {
+    padding: 5,
+    borderWidth: 2,
+    borderColor: '#525759',
+    borderRadius: 80,
+    height: 100,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  iconPlus: {
+    left: 35,
+    backgroundColor: '#00ACEE',
+    borderRadius: 40,
+  },
+  
+  header: {
+    alignItems: 'center',
+    marginBottom: 15
+  },
+
+  headerTitle: {
+    fontWeight: 'bold',
+    fontFamily: 'Roboto_700Bold',
+    fontSize: 25,
+    marginBottom: 8,
+    marginTop: 18
+  },
+
+  headerText: {
+    textAlign: 'center',
+    color: '#73797d',
+    fontSize: 14,
+    marginBottom: 8,
+    marginRight: 7,
+    marginLeft: 7,
+  },
+
+
+
 });
